18058c4ba79fe2631d7acbef474fd21738c15f17
Kyle Kingsberry <kkingsbe@gmail.com>
2023-07-06 14:06:47 -0400

    Set title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Tigger</title>
       <body className={inter.className}>{children}</body>
     </html>
   )
