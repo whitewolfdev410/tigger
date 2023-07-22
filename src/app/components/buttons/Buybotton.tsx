b2cf1db1e38767848ad2f7ffdbd138f0da60033d
kkingsbe <kkingsbe@gmail.com>
2023-07-22 15:00:23 -0400

    Update Buybotton.tsx

--- src/app/components/buttons/Buybotton.tsx
+++ src/app/components/buttons/Buybotton.tsx
@@ -7,6 +7,7 @@ import { mainnet } from "wagmi/chains";
 interface Props {
   bgColor: string;
   ftColor: string;
+  text: string;
 }
 const chains = [mainnet];
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "";
@@ -16,7 +17,7 @@ const wagmiConfig = createConfig({
   connectors: w3mConnectors({ projectId, chains }),
   publicClient,
 });
-const BuyButton: React.FC<Props> = ({ bgColor, ftColor }) => {
+const BuyButton: React.FC<Props> = ({ bgColor, ftColor, text }) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
@@ -24,7 +25,7 @@ const BuyButton: React.FC<Props> = ({ bgColor, ftColor }) => {
         className={`bg-${bgColor} !important text-center px-[30px] text-base text-${ftColor} Orbitron px-[30px] font-black py-4 rounded-lg uppercase m-3`}
         onClick={() => setShowModal(true)}
       >
-        Buy on Uniswap
+        {text}
       </div>
       <WagmiConfig config={wagmiConfig}>
         {showModal && (
